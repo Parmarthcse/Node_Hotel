5afc2d515b78f65b6678a4bf977dc9351705bcd5
Parmarthcse <gambhirsujal22@gmail.com>
2024-10-22 11:03:05 +0530

    commit

--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const personSchema = new mongoose.Schema({
     name:{
         type: String,
